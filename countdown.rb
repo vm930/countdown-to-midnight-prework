92b2ff90290019503abecedc9bb879a47279ff14
abstract-animation-9914 <vm930@hotmail.com>
2019-01-01 21:31:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
-
-def countdown
+  counter = 0
+def countdown (num)
+    number_sec = num
+    while counter < num do
+    puts "#{number_sec} SECOND(S)!"
+    countdown_with_sleep(num)
+    counter+=1
+    number_sec = num -counter
+    end 
+  puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (int)
+  sleep(int);
+end 
