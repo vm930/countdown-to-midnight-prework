9adacd4589445f10442cd6aa818a0d96c43fa9b2
abstract-animation-9914 <vm930@hotmail.com>
2019-01-01 21:45:11 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
-
-def countdown
+  
+def countdown (num)
+  counter = 0
+  secondsleft = num
+    while counter < num 
+    puts "#{secondsleft} SECOND(S)!"
+    countdown_with_sleep(1)
+    counter +=1
+    secondsleft = num -1
+    end 
+  puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (1)
+  sleep(1);
+end 
